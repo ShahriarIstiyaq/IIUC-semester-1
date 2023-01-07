beb3e72e6be47a390faa7279b9961c0aaa8f8ff2
ShahriarIstiyaq <122169844+ShahriarIstiyaq@users.noreply.github.com>
2023-01-07 22:51:40 +0600

    Add files via upload

---
+++ New start/Escape_sequence.c
@@ -0,0 +1,13 @@
+//This is a single line comment.
+/*This is 
+multiline comment.*/
+#include<stdio.h>
+int main()
+{
+    //Backslash(\) is used as escape sequence character.
+    printf("My name is Istiyaq.\b \aMy number is 018******** \n");
+    printf("\nMy name is S.\r My number is 018********");
+
+    return 0;
+
+}
