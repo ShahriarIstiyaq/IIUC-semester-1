4494ea1fab2a44ea9aa252a7c15f2b726dcb0f88
ShahriarIstiyaq <122169844+ShahriarIstiyaq@users.noreply.github.com>
2023-01-08 23:24:38 +0600

    Add files via upload

---
+++ beecrowd_promblems/p-1013.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    //Formula maiorAB = (a+b+abs(a-b))
+    int a, b, c, d;
+    scanf("%d %d %d", &a, &b, &c);
+    d = (a+b+abs(a-b))/2;
+    d = (c+d+abs(c-d))/2;
+    printf("%d eh o maior\n", d);
+
+    return 0;
+
+}
