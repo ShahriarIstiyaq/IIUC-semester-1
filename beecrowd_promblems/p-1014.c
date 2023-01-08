1f3f016a94d1a99a4347a4ce3a2aceea049fe90d
ShahriarIstiyaq <122169844+ShahriarIstiyaq@users.noreply.github.com>
2023-01-08 23:35:41 +0600

    Update p-1014.c

--- beecrowd_promblems/p-1014.c
+++ beecrowd_promblems/p-1014.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    //avg_consumption = distance/total used fule.
+    //avg_consumption = distance/total used fuel.
     double x,y;
     scanf("%lf %lf", &x, &y);
     printf("%0.3lf km/l\n", x/y);
