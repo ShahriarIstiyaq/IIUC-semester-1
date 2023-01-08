273c915153b19c0fe7c5247bb7f2fb29ce27328a
ShahriarIstiyaq <122169844+ShahriarIstiyaq@users.noreply.github.com>
2023-01-09 00:48:06 +0600

    Add files via upload

---
+++ beecrowd_promblems/p-1016.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    //The car y travels 1km in 2min.So, the problem is to solve how min it will take to reach Y the given point.
+    int y;
+    scanf("%d", &y);
+    printf("%d minutos\n", y*2);
+
+    return 0;
+}
